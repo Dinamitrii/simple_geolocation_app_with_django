2d957e0f2b0ecfa0585f9093dde116cd17e8676d
dinamitrii <k_karaivanov@abv.bg>
2025-08-20 07:03:54 +0300

    adaptive

--- location/views.py
+++ location/views.py
@@ -16,12 +16,28 @@ def index(request):
     location_text = res.text
     location_json = json.loads(location_text)
 
-    lat = location_json['lat']
-    lon = location_json['lon']
-
-
-    weather = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={"API_KEY"}')
-    weather_json = json.loads(weather.text)
+    # lat = location_json['lat']
+    # lon = location_json['lon']
+    #
+    #
+    # weather = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={"API_KEY"}')
+    # weather_json = json.loads(weather.text)
 
     
     return render(request, 'index.html', {'data': location_json})
+
+
+def weather(request):
+
+    lat = index(request).get('lat', None)
+    lon = index(request).get('lon', None)
+    
+
+    weather = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={"API_KEY"}')
+
+    weather_json = json.loads(weather.text)
+
+    return render(request, 'weather.html.bck', {'data': weather_json})
+
+
+
